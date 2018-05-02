9c7a98df5c2ce7f1dc668450bf658376c9382276
otaon <16165624+otaon@users.noreply.github.com>
2018-05-03 00:55:53 +0900

    F ランダムなエッジのリストを生成する関数群を追加

--- gtw.lisp
+++ gtw.lisp
@@ -10,4 +10,23 @@
 (defparameter *worm-num* 3)     ; ギャング Gluesome Glowworm のチーム数
 (defparameter *cop-odds* 15)    ; 検問のある確率(1/*cop-odds*の確率)
 
+(defun random-node ()
+  "ランダムなノード番号を返す
+   ノード番号は1始まり"
+  (1+ (random *node-num*)))
+
+(defun edge-pair (a b)
+  "ノードaとノードb(a!=b)を dotted pair として両方の向きでリストとする
+   a: ノード番号
+   b: ノード番号
+   > ((a . b) (b . a))"
+  (unless (eql a b)
+    (list (cons a b) (cons b a))))
+
+(defun make-edge-list ()
+  "ランダムなエッジのリストを生成する
+   ノードが重複するとエッジを作成しないため、
+   エッジ数は*edge-num*と等しくならない場合がある"
+  (apply #'append (loop repeat *edge-num*
+                        collect (edge-pair (random-node) (random-node)))))
 
