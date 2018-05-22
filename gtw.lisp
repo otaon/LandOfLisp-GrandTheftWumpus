4748b8ba7a6665dd9ac55db6aef8c2f6e73effc7
otaon <16165624+otaon@users.noreply.github.com>
2018-05-23 00:02:43 +0900

    R vimのfold用makerを追加

--- gtw.lisp
+++ gtw.lisp
@@ -18,7 +18,7 @@
 
 
 ;; リストをハッシュテーブルに変換する
-(defun hash-edges (edge-list)
+(defun hash-edges (edge-list);{{{
   "リストの全てのキーとバリューをハッシュテーブルにプッシュする"
   (let ((tab (make-hash-table)))    ; tab: ハッシュテーブル
     (mapc (lambda (x)
@@ -27,43 +27,43 @@
               ;; gethash node tab: ハッシュテーブルにおいてキーがnodeの場所
               (push (cdr x) (gethash node tab))))
           edge-list)
-    tab))
+    tab));}}}
 
 
 ;; ランダムなエッジの生成
-(defun random-node ()
+(defun random-node ();{{{
   "ランダムなノード番号を返す
    ノード番号は1始まり"
-  (1+ (random *node-num*)))
+  (1+ (random *node-num*)));}}}
 
-(defun edge-pair (a b)
+(defun edge-pair (a b);{{{
   "ノードaとノードb(a!=b)を dotted pair として両方の向きでリストとする
    a: ノード番号
    b: ノード番号
    > ((a . b) (b . a))"
   (unless (eql a b)
-    (list (cons a b) (cons b a))))
+    (list (cons a b) (cons b a))));}}}
 
-(defun make-edge-list ()
+(defun make-edge-list ();{{{
   "ランダムなエッジのリストを生成する
    始点と終点が同じノードだとエッジを作成しないため、
    エッジ数は*edge-num*と等しくならない場合がある
    さらに、完全一致するエッジが生成される可能性もある"
   (apply #'append (loop repeat *edge-num*
-                        collect (edge-pair (random-node) (random-node)))))
+                        collect (edge-pair (random-node) (random-node)))));}}}
 
 
 ;; 孤島を作らない
-(defun direct-edges (node edge-list)
+(defun direct-edges (node edge-list);{{{
   "nodeと直接つながっているエッジをedge-listから抽出する
    node: 起点となるノード B
    edge-list: 全エッジのリスト ((A.B)(B.A)(B.C)(C.B))
    return: nodeと直接つながっているエッジのリスト ((B.A)(B.C))"
   (remove-if-not (lambda (x)
                    (eql (car x) node))
-                 edge-list))
+                 edge-list));}}}
 
-(defun get-connected (node edge-list)
+(defun get-connected (node edge-list);{{{
   "繋がっているノードを探す
    node: 出発点となるノード A
    edge-list: 全エッジのリスト ((A.B)(B.A)(B.C)(C.B)(D.E)(E.D))
@@ -80,9 +80,9 @@
                          (traverse (cdr edge)))
                        (direct-edges node edge-list)))))
       (traverse node))
-    visited))
+    visited));}}}
 
-(defun get-connected-hash (node edge-tab)
+(defun get-connected-hash (node edge-tab);{{{
   "繋がっているノードを探す（ハッシュテーブル版）"
   (let ((visited (make-hash-table)))
     (labels ((traverse (node)
@@ -92,9 +92,9 @@
                          (traverse edge))
                        (gethash node edge-tab)))))
       (traverse node))
-    visited))
+    visited));}}}
 
-(defun find-islands (nodes edge-list)
+(defun find-islands (nodes edge-list);{{{
   "街に存在する島を列挙する
    nodes: 全てのノードのリスト  (ABCDE)
    edge-list: 全てのエッジのリスト ((A.B)(B.A)(B.C)(C.B)(D.E)(E.D))
@@ -114,26 +114,26 @@
                  (when unconnected
                    (find-island unconnected)))))
       (find-island nodes))
-    islands))
+    islands));}}}
 
-(defun connect-with-bridges (islands)
+(defun connect-with-bridges (islands);{{{
   "2つ以上島がある場合、それらの島に橋を架ける
    islands: 島のリスト  ((ABC)(DE))
    return: 橋となるエッジのリスト   ((A.D)(D.A))"
   (when (cdr islands)
     (append (edge-pair (caar islands) (caadr islands))
-            (connect-with-bridges (cdr islands)))))
+            (connect-with-bridges (cdr islands)))));}}}
 
-(defun connect-all-islands (nodes edge-list)
+(defun connect-all-islands (nodes edge-list);{{{
   "街に存在する島々に橋を架ける
    nodes: 全てのノードのリスト  (ABCDE)
    edge-list: 全てのエッジのリスト  ((A.B)(B.A)(B.C)(C.B)(D.E)(E.D))
    return: 橋を架けた後のエッジのリスト ((A.D)(D.A)(A.B)(B.A)(B.C)(C.B)(D.E)(E.D))"
-  (append (connect-with-bridges (find-islands nodes edge-list)) edge-list))
+  (append (connect-with-bridges (find-islands nodes edge-list)) edge-list));}}}
 
 
 ;; congestion city のためのエッジリストを完成させる
-(defun make-city-edges ()
+(defun make-city-edges ();{{{
   "congestion city のエッジリストを作成する"
   ;; nodes: 1から*node-num*までのノード番号のリスト
   ;; edge-list: 街の全ノードに接続されたエッジのリスト
@@ -146,9 +146,9 @@
                                 (declare (ignore x))
                                 (zerop (random *cop-odds*)))
                               edge-list)))
-    (add-cops (edges-to-alist edge-list) cops)))
+    (add-cops (edges-to-alist edge-list) cops)));}}}
 
-(defun edges-to-alist (edge-list)
+(defun edges-to-alist (edge-list);{{{
   "エッジをalistに変換する
    edge-list: ((1.2)(2.1)...(a.b)(b.a)) 形式のエッジのリスト
    return: ((1 (2)) (2 (1) (3)) (3 (2))) 形式のエッジのalist"
@@ -162,9 +162,9 @@
                           (remove-duplicates (direct-edges node1 edge-list)
                                              :test #'equal))))
           ;; エッジの起点となるノードのリスト(1 2 3 ...)
-          (remove-duplicates (mapcar #'car edge-list))))
+          (remove-duplicates (mapcar #'car edge-list))));}}}
 
-(defun add-cops (edge-alist edges-with-cops)
+(defun add-cops (edge-alist edges-with-cops);{{{
   "alistに検問所の情報を追加する
    edge-alist: 街のエッジのリスト ((1 (2)) (2 (1) (3)) (3 (2)))
    edges-with-cops: 検問所のあるエッジのリスト
@@ -181,26 +181,26 @@
                                     (list node2 'cops)
                                     edge)))
                             node1-edges))))
-          edge-alist))
+          edge-alist));}}}
 
 
 ;; congestion cityのノードリストを作る
-(defun neighbors (node edge-alist)
+(defun neighbors (node edge-alist);{{{
   "nodeに直接繋がっているノードのリストを返す
    node: 基準となるノード 2
    edge-alist: 街のエッジのリスト ((1 (2)) (2 (1) (3)) (3 (2)))
    return: 直接繋がっているノードのリスト (1 3)"
-  (mapcar #'car (cdr (assoc node edge-alist))))
+  (mapcar #'car (cdr (assoc node edge-alist))));}}}
 
-(defun within-one (a b edge-alist)
+(defun within-one (a b edge-alist);{{{
   "ノードbが他ノードaと隣同士で繋がっているかを返す
    a: 基準となるノード 1
    b: 隣同士で繋がっているか判定したいノード 3
    edge-alist: 街のエッジのリスト ((1 (2)) (2 (1) (3)) (3 (2)))
    return: 隣同士で繋がっているかの真偽値 nil"
-  (member b (neighbors a edge-alist)))
+  (member b (neighbors a edge-alist)));}}}
 
-(defun within-two (a b edge-alist)
+(defun within-two (a b edge-alist);{{{
   "ノードbが隣同士で、または、他ノードを挟んでノードaと繋がっているかを返す
    a: 基準となるノード 1
    b: 隣同士で、または、他ノードを挟んで繋がっているか判定したいノード 3
@@ -210,9 +210,9 @@
       ;; aの隣のノードの中で、bの隣のノードが1つ以上存在するか
       (some (lambda (x)
               (within-one x b edge-alist))
-            (neighbors a edge-alist))))
+            (neighbors a edge-alist))));}}}
 
-(defun make-city-nodes (edge-alist)
+(defun make-city-nodes (edge-alist);{{{
   "congestion city の最終的なマップを返す
    edge-alist: 街のエッジのリスト
    return: 情報つきの街の全ノードのリスト"
@@ -238,38 +238,38 @@
                                  '(lights!)))
                           ;; 警察が隣接する道路（エッジ）にいるノードには「sirens!」情報を追加
                           (when (some #'cdr (cdr (assoc n edge-alist)))
-                            '(sirens!))))))
+                            '(sirens!))))));}}}
 
 
 ;; gtwを初期化する
-(defun new-game ()
+(defun new-game ();{{{
   "ゲームを初期化する"
   (setf *congestion-city-edges* (make-city-edges))
   (setf *congestion-city-nodes* (make-city-nodes *congestion-city-edges*))
   (setf *player-pos* (find-empty-node))
   (setf *visited-nodes* (list *player-pos*))
   (draw-city)
-  (draw-known-city))
+  (draw-known-city));}}}
 
-(defun find-empty-node ()
+(defun find-empty-node ();{{{
   "プレーヤーが敵と同じ場所に配置されないように、空のノードを探し出す
    return: 情報が追加されていないノードの番号"
   (let ((x (random-node)))
     (if (cdr (assoc x *congestion-city-nodes*))
         (find-empty-node)
-        x)))
+        x)));}}}
 
 
 ;; congestion cityのマップを描く
-(defun draw-city ()
+(defun draw-city ();{{{
   "graphvizを使ってcongestion cityのマップを描く"
   (ugraph->png (concatenate 'string *game-directory* "/" "city")
                *congestion-city-nodes*
-               *congestion-city-edges*))
+               *congestion-city-edges*));}}}
 
 
 ;; 部分的な知識から congestion city を描く
-(defun known-city-nodes ()
+(defun known-city-nodes ();{{{
   "既知のノードからなるalistを作る"
   ;; 既に訪れたノード&そこに直接繋がったノード(?と*付き)のリスト
   (mapcar (lambda (node)
@@ -296,9 +296,9 @@
                     (mapcan (lambda (node)
                               (mapcar #'car
                                       (cdr (assoc node *congestion-city-edges*))))
-                            *visited-nodes*)))))
+                            *visited-nodes*)))));}}}
 
-(defun known-city-edges ()
+(defun known-city-edges ();{{{
   "まだ訪れていない道にいる警官のサイレンの情報を取り除いたエッジのalistを作る"
   (mapcar (lambda (node)
             ;; すべての既に訪れたノードに対して警察のサイレンの情報を消すか否か操作する
@@ -313,25 +313,25 @@
                                      (list (car x))))
                                ;; 既に訪れたノードに直接繋がったリスト
                                (cdr (assoc node *congestion-city-edges*)))))
-          *visited-nodes*))
+          *visited-nodes*));}}}
 
-(defun draw-known-city ()
+(defun draw-known-city ();{{{
   "既知の部分だけの地図を描く"
   (ugraph->png (concatenate 'string *game-directory* "/" "known-city")
                (known-city-nodes)
-               (known-city-edges)))
+               (known-city-edges)));}}}
 
 
 ;; 街を歩き回る
-(defun walk (pos)
+(defun walk (pos);{{{
   "ただ移動するだけ"
-  (handle-direction pos nil))
+  (handle-direction pos nil));}}}
 
-(defun charge (pos)
+(defun charge (pos);{{{
   "移動しつつ残った最後の弾丸で攻撃する"
-  (handle-direction pos t))
+  (handle-direction pos t));}}}
 
-(defun handle-direction (pos charging)
+(defun handle-direction (pos charging);{{{
   "posが移動できるノードなら移動する
    攻撃を選択していたら攻撃する"
   ;; 現在位置からposへ行く道があれば、移動と、場合によっては攻撃する
@@ -340,9 +340,9 @@
                      (cdr (assoc *player-pos* *congestion-city-edges*)))))
     (if edge
         (handle-new-place edge pos charging)
-        (princ "That location does not exist!"))))
+        (princ "That location does not exist!"))));}}}
 
-(defun handle-new-place (edge pos charging)
+(defun handle-new-place (edge pos charging);{{{
   "posへ移動する。cop, wumpus, glow wormの有無によってイベントメッセージを表示する"
   ;; node:     移動先のノード情報
   ;; has-worm: glow wormがいるか否かのフラグ
@@ -370,7 +370,7 @@
       (has-worm (let ((new-pos (random-node)))
                   (princ "You ran into a Glow Worm Gang! You're now at ")
                   (princ new-pos)
-                  (handle-new-place nil new-pos nil))))))
+                  (handle-new-place nil new-pos nil))))));}}}
 
 
 ;;; ---------------------------------------------------------------
